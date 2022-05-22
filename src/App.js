506fafbb190b94e80e95ebf5c55a5f72adca7605
Huraira78 <2019cs661@student.uet.edu.pk>
2022-05-22 20:50:50 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./eating_app.css";
+import { data } from "./data.js";
 
-function App() {
+const Eating_app = () => {
+  const [first, setfirst] = useState(data);
+  const filterItem = (currCatagory) => {
+    const khana = data.filter((elem) => {
+      return elem.catagory === currCatagory;
+    });
+    setfirst(khana);
+  };
+  const filterSearch=(e)=>{
+    const SearchFilter=data.filter(elem=>
+      elem.catagory.toLowerCase().includes(e.target.value.toLowerCase())
+    )
+    setfirst(SearchFilter)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <div className="header">
+        <h1>Eating Menu Dashoard</h1>
+      </div>
+      <div className="btns">
+        <a href="#">
+          <button onClick={() => filterItem("breakfast")}>Breakfast</button>
         </a>
-      </header>
+        <a href="#">
+          <button onClick={() => filterItem("Lunch")}>Lunch</button>
+        </a>
+        <a href="#">
+          <button onClick={() => filterItem("Dinner")}>Dinner</button>
+        </a>
+        <a href="#">
+          <button onClick={() => setfirst(data)}>All</button>
+        </a>
+      </div>
+      <div className="input">
+        <input type="text" className="search" placeholder="Search..."  onChange={(e)=>filterSearch(e)}/>
+      </div>
+      <div className="mappingData">
+        {first.map((elem) => {
+          const { id, name, price, catagory, ImageUrl } = elem;
+          return (
+            <div className="main">
+              <div className="items" key={id}>
+                <div className="image">
+                  <img src={ImageUrl} alt={name} />
+                </div>
+                <div className="credentials">
+                  <h1>{name}</h1>
+                  <h2>{catagory}</h2>
+                  <h3>{price}</h3>
+                </div>
+              </div>
+             </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Eating_app;
